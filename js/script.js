d04d0ea738753c36313e4104f17c2d3914248493
Ewa Madej-Pawlowska <ewa.madej@gmail.com>
2022-09-13 18:15:21 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+{
+
+    const tasks = [
+        {
+            content: "test",
+            done: false,
+        },
+        {
+            content: "test",
+            done: true,
+        },
+    ];
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+                <li>
+                ${task.content}
+                </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+    };
+
+    const init = () => {
+        render();
+    };
+
+    init();
+
+}
