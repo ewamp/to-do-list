b5cb645b6bc2ccaa86db9610f198d1fa61753e5b
Ewa Madej-Pawlowska <ewa.madej@gmail.com>
2022-09-14 10:54:25 +0200

    remove tasks

--- js/script.js
+++ js/script.js
@@ -11,12 +11,28 @@
         },
     ];
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    };
+
     const render = () => {
         let htmlString = "";
 
         for (const task of tasks) {
             htmlString += `
                 <li${task.done ? " style=\"text-decoration: line-through\"" : ""}>
+
+                <button class="js-remove">usuń</button>
+
                 ${task.content}
                 </li>
             `;
@@ -24,14 +40,14 @@
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
 
-    };
+        const removeButtons = document.querySelectorAll(".js-remove");
 
-    const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
         });
 
-        render();
     };
 
     const onFormSubmit = (event) => {
